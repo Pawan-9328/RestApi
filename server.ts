fa5c9c53f9ca5a50704c90ab6046eb0385a30995
Pawan Gola <pawangola908@gmail.com>
2024-04-12 07:13:44 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,14 @@
-console.log("Welcome to e book apis");
+import app from "./src/app";
+
+const startServer = () =>{
+     const port = process.env.PORT || 3000;
+
+
+     app.listen(port, () =>{
+         console.log(`Listening on port: ${port}`);
+         
+     });
+};
+//if you can't pass this then server start 3000 port automatically 
+
+startServer();
